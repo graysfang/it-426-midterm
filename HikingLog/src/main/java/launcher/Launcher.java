1774679f0c0facd2c99d0b7011565e6f3cd8fc8c
Nicholas Perez <nperez9@mail.greenriver.edu>
2017-10-30 13:16:01 -0700

    Initial package and files.

---
+++ HikingLog/src/main/java/launcher/Launcher.java
@@ -0,0 +1,18 @@
+package launcher;/*
+ * Nicholas Perez
+ * 10/30/2017
+ * Launcher.java
+ *
+ * DESCRIPTION
+ */
+
+/**
+ * DESCRIPTION
+ *
+ * @author Nicholas Perez
+ * @version 1.0
+ **/
+public class Launcher
+{
+
+}
