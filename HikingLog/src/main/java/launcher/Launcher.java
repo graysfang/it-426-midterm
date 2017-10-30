dceb3d9723fe611b7689015ff0d7b239f22acafb
Nicholas Perez <nperez9@mail.greenriver.edu>
2017-10-30 13:26:25 -0700

    Started JavaFX Homepage.

--- HikingLog/src/main/java/launcher/Launcher.java
+++ HikingLog/src/main/java/launcher/Launcher.java
@@ -1,10 +1,14 @@
-package launcher;/*
+package launcher;
+/*
  * Nicholas Perez
  * 10/30/2017
  * Launcher.java
  *
  * DESCRIPTION
  */
+
+import javafx.application.Application;
+import ui.HikeLogUI;
 
 /**
  * DESCRIPTION
@@ -14,5 +18,8 @@ package launcher;/*
  **/
 public class Launcher
 {
-
+    public static void main(String[] args)
+    {
+        Application.launch(HikeLogUI.class, args);
+    }
 }
--- HikingLog/src/main/java/ui/HikeLogUI.java
+++ HikingLog/src/main/java/ui/HikeLogUI.java
@@ -7,12 +7,30 @@ package ui;
  * DESCRIPTION
  */
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * DESCRIPTION
  *
  * @author Nicholas Perez
  * @version 1.0
  **/
-public class HikeLogUI {
+public class HikeLogUI extends Application
+{
 
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        stage.setTitle("Hiking Log");
+        stage.setScene(assemble());
+        stage.show();
+    }
+
+    private Scene assemble()
+    {
+        
+        return  null;
+    }
 }
