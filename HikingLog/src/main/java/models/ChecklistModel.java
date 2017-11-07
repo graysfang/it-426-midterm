24dd96ebf17bd9215c7efd1fc6290769ab3988d5
Nicholas Perez <nperez9@mail.greenriver.edu>
2017-11-07 10:13:41 -0800

    Finished the functionality for checklist and started working on refactoring other ui/model classes for layout and css.

--- HikingLog/src/main/java/models/ChecklistModel.java
+++ HikingLog/src/main/java/models/ChecklistModel.java
@@ -1,32 +1,66 @@
-package models;/*
- * Nicholas Perez
+package models;
+
+/*
+ * Nicholas Perez, Hillary Wagoner, Bo Zhang
  * 11/6/2017
  * ChecklistModel.java
  *
- * DESCRIPTION
+ * Model/Controller for the checklist.
  */
 
 import java.util.ArrayList;
 
 /**
- * DESCRIPTION
+ * Model/Controller for the checklist.
  *
- * @author Nicholas Perez
+ * @author Nicholas Perez, Hillary Wagoner, Bo Zhang
  * @version 1.0
  **/
 public class ChecklistModel
 {
-    private ArrayList<String> checklist = UtilitiesIO.readIn("checklist.txt");
+    private static ArrayList<String> checklist = UtilitiesIO.readIn("checklist.txt");
 
+    /**
+     * Gets the checklist
+     * @return
+     */
     public ArrayList getChecklist()
     {
         return checklist;
     }
 
-    public void setChecklist()
+    /**
+     * Adds a new item to the checklist
+     * @param newItem the items to be added
+     */
+    public void setChecklist(String newItem)
     {
-        
+        checklist.add(newItem);
+
+        UtilitiesIO.writeOut("checklist.txt", checklist);
     }
 
+    /**
+     * removes an item from the checklist
+     * @param removeItem the item to be removed
+     */
+    public void removeItem(String removeItem)
+    {
+        for (String item: checklist)
+        {
+            if(item.toLowerCase().equals(removeItem.toLowerCase()))
+            {
+                checklist.remove(removeItem);
+                break;
+            }
+        }
+        UtilitiesIO.writeOut("checklist.txt", checklist);
+        updateList();
+    }
 
+    //helper method to update the last after item removal
+    private void updateList()
+    {
+        checklist = UtilitiesIO.readIn("checklist.txt");
+    }
 }
